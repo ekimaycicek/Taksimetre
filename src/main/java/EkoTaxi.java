a8a44924ce5970d99ddb92572553f47939c55d13
ekimaycicek <ekimaycicek@icloud.com>
2023-09-07 13:20:27 +0300

    new

---
+++ src/main/java/EkoTaxi.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class EkoTaxi {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the distance traveled in kilometers: ");
+        double distance = input.nextDouble();
+
+        double totalFee = calculateFee(distance);
+        System.out.println("Total Fee: " + totalFee + " TL");
+
+
+    }
+
+    public static double calculateFee(double distance) {
+        double beginning = 8.00;
+        double kmPer = 12.50;
+
+        double totalFee = beginning + (kmPer * distance);
+        return totalFee;
+    }
+}
